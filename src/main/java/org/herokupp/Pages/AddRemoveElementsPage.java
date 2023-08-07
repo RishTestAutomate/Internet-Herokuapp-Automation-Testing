abfc61ada092c134a7b61ab940f977ceb6fd47a6
Rishabh Sharma <rishabhssharma2607@gmail.com>
2023-08-07 19:46:05 +0530

    Initial commit

---
+++ src/main/java/org/herokupp/Pages/AddRemoveElementsPage.java
@@ -0,0 +1,35 @@
+package org.herokupp.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class AddRemoveElementsPage extends BasePage{
+
+    @FindBy(xpath = "//button[text()='Add Element']")
+    public WebElement addElement;
+
+    @FindBy(xpath = "//button[text()='Delete']")
+    public WebElement deleteElement;
+
+    @FindAll({@FindBy(xpath = "//button[@class='added-manually']")})
+    public List<WebElement> deleteAllElement;
+
+
+    public AddRemoveElementsPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickOnAddElement(){
+        addElement.click();
+    }
+
+    public void clickOnRemoveElement(){
+        deleteElement.click();
+    }
+}
---
+++ src/main/java/org/herokupp/Pages/BasePage.java
@@ -0,0 +1,27 @@
+package org.herokupp.Pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    private WebDriver driver;
+    private JavascriptExecutor jse;
+
+    @FindBy(xpath = "//a[text()='Add/Remove Elements']")
+    public WebElement addRemoveElement;
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        jse = (JavascriptExecutor) driver;
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    public void visitAddRemoveElement(){
+        addRemoveElement.click();
+    }
+}
