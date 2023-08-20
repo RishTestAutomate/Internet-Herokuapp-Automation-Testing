6ae9e7f54f4b5c3c0667807802adb1058bbd1382
Rishabh Sharma <rishabhssharma2607@gmail.com>
2023-08-20 22:14:25 +0530

    Large&Deep DOM Page is tested.

--- src/test/java/org/herokupp/PageTests/LargeAndDeepDomTests.java
+++ src/test/java/org/herokupp/PageTests/LargeAndDeepDomTests.java
@@ -1,20 +1,15 @@
 package org.herokupp.PageTests;
 
-import Utilities.Utils;
-import org.herokupp.Pages.BasePage;
-import org.herokupp.Pages.KeyPressesPage;
 import org.herokupp.Pages.LargeAndDeepDomPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.text.DecimalFormat;
 import java.util.List;
 
 public class LargeAndDeepDomTests {
